afef063878a6d34b2806d37246d8cc5b81181a6c
Khaled El Hamzi <contact@elhamzi.me>
2021-08-13 01:49:58 +0200

    Get rid of procedural paradigm

--- src/me/elhamzi/Main.java
+++ src/me/elhamzi/Main.java
@@ -5,16 +5,12 @@ public class Main {
     final static byte PERCENT = 100;
 
     public static void main(String[] args) {
-
-        int principal = (int) Console.readValue("Principal: ");
-/*
         int principal = (int) Console.readValue("Principal($1k - $1M): ", 1000, 1_000_000);
-*/
         float annualInterest = (float) Console.readValue("Annual Interest Rate: ", 0, 30);
         byte years = (byte) Console.readValue("Period(Years): ", 1, 30);
 
-        new MortgageReport(calculator).printMortgage(principal, annualInterest, years);
-        new MortgageReport(calculator).printPaymentSchedule();
+        new MortgageReport(new MortgageCalculator(principal, annualInterest,years)).printMortgage();
+        new MortgageReport(new MortgageCalculator(principal, annualInterest,years)).printPaymentSchedule();
     }
 
 }
