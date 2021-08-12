987b0c2baeb083c76bfd9e8c3b7a6acf59599a8a
Khaled El Hamzi <contact@elhamzi.me>
2021-08-13 01:22:50 +0200

    Refactor mortgage calculator class

--- src/me/elhamzi/MortgageCalculator.java
+++ src/me/elhamzi/MortgageCalculator.java
@@ -1,21 +1,31 @@
 package me.elhamzi;
 
 public class MortgageCalculator {
-    public static Double calculateMortgage(int principal, float annualInterest, byte years) {
-        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
-        short numberOfPayments = (short) (years * Main.MONTHS_IN_YEAR);
+    private int _principal;
+    private float _annualInterest;
+    private byte _years;
 
-        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)) / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+    public MortgageCalculator(int principal, float annualInterest, byte years) {
+        this._principal = principal;
+        this._annualInterest = annualInterest;
+        this._years = years;
+    }
+
+    public double calculateMortgage() {
+        float monthlyInterest = _annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
+        short numberOfPayments = (short) (_years * Main.MONTHS_IN_YEAR);
+
+        double mortgage = _principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)) / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
         // M=P*[(r(1+r)^n)/((1+r)^n-1)].  // https://www.wikihow.com/Calculate-Mortgage-Payments
 
         return mortgage;
     }
 
-    public static double calculateLoanBalance(int principal, float annualInterest, byte years, short numberOfPaymentsMade) {
-        float monthlyInterest = annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
-        short numberOfPayments = (short) (years * Main.MONTHS_IN_YEAR);
+    public double calculateLoanBalance(short numberOfPaymentsMade) {
+        float monthlyInterest = _annualInterest / Main.PERCENT / Main.MONTHS_IN_YEAR;
+        short numberOfPayments = (short) (_years * Main.MONTHS_IN_YEAR);
 
-        double loanBalance = principal * ((Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade)) / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1));
+        double loanBalance = _principal * ((Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade)) / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1));
         // B = L[(1 + c)^n - (1 + c)p]/[(1 + c)^n - 1] // https://www.mtgprofessor.com/formulas.htm
 
         return loanBalance;
--- src/me/elhamzi/MortgageReport.java
+++ src/me/elhamzi/MortgageReport.java
@@ -5,16 +5,18 @@ import java.util.Locale;
 
 public class MortgageReport {
     public static void printMortgage(int principal, float annualInterest, byte years) {
-        double mortgage = MortgageCalculator.calculateMortgage(principal, annualInterest, years);
+        var calculator = new MortgageCalculator(principal, annualInterest, years);
+        double mortgage = calculator.calculateMortgage();
         String mortgageFormatted = NumberFormat.getCurrencyInstance(Locale.US).format(mortgage);
         System.out.println("\nMORTGAGE\n--------");
         System.out.println("Monthly Payments: " + mortgageFormatted + "\n");
     }
 
     public static void printPaymentSchedule(int principal, float annualInterest, byte years) {
+        var calculator = new MortgageCalculator(principal, annualInterest, years);
         System.out.println("PAYMENT SCHEDULE\n----------------");
         for (short month = 1; month <= years * Main.MONTHS_IN_YEAR; month++) {
-            double balance = MortgageCalculator.calculateLoanBalance(principal, annualInterest, years, month);
+            double balance = calculator.calculateLoanBalance(month);
             String loanBalance = NumberFormat.getCurrencyInstance(Locale.US).format(balance);
             System.out.println("Month (" + month + "):" + loanBalance);
         }
