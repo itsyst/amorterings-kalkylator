180574a7d32630c9afc6b07163e3449cbff7cc46
Khaled El Hamzi <contact@elhamzi.me>
2021-08-13 03:16:17 +0200

    Extract currency field

--- src/me/elhamzi/MortgageReport.java
+++ src/me/elhamzi/MortgageReport.java
@@ -6,22 +6,24 @@ import java.util.Locale;
 public class MortgageReport {
 
     private MortgageCalculator _calculator;
+    private final NumberFormat _currency;
 
     public MortgageReport(MortgageCalculator calculator) {
         this._calculator = calculator;
+        this._currency = NumberFormat.getCurrencyInstance(Locale.US);
     }
 
     public void printPaymentSchedule() {
         System.out.println("PAYMENT SCHEDULE\n----------------");
         for (double balance : _calculator.getRemainingBalances()) {
-            String loanBalance = NumberFormat.getCurrencyInstance(Locale.US).format(balance);
+            String loanBalance = _currency.format(balance);
             System.out.println("Month: " + loanBalance);
         }
     }
 
     public void printMortgage() {
         double mortgage = _calculator.calculateMortgage();
-        String mortgageFormatted = NumberFormat.getCurrencyInstance(Locale.US).format(mortgage);
+        String mortgageFormatted = _currency.format(mortgage);
         System.out.println("\nMORTGAGE\n--------");
         System.out.println("Monthly Payments: " + mortgageFormatted + "\n");
     }
